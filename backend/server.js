8143e68e81926a6f944714b171a7380f55109529
sohail-op <67971304+sohail-op@users.noreply.github.com>
2024-03-29 12:18:25 +0530

    Change API endpoint for fetching text to use leading slash
    The fetch request to the API endpoint for getting text has been updated to include a leading slash before the endpoint path. This change was made to ensure the request is correctly routed to the API server.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+// import path from "path";
 
 import router from "./routes/textRoute.js";
 import connectDb from "./config/dbConnection.js";
@@ -12,11 +13,24 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5001;
 
+// const __dirname = path.resolve();
+
 app.use(cors());
 
 app.use(express.json());
+
 app.use("/api", router);
+app.use("/", (res, req) => {
+  res.send("Welcome to Home");
+});
+
 app.get(errorHandler);
+
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
 
 app.listen(port, () => {
   console.log(`Server is Listening on http://localhost:${port}`);
