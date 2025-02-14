45216fbc969e7a3b1409858fd3544357d2cd373f
sohail-op <67971304+sohail-op@users.noreply.github.com>
2025-02-15 01:25:30 +0530

    Add: add new file to establish socket connection

---
+++ backend/socket/socket.js
@@ -0,0 +1,30 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "DELETE"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected: ", socket.id);
+
+  socket.on('getCode', (code)=>{
+    console.log(code);
+  }
+)
+
+socket.on('disconnect', () => {
+  console.log(`User disconnected: ${socket.id}`);
+});
+
+});
+
+export { app, server, io };
