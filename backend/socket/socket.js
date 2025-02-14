4d7339ba75563263be665c500a4268ecb6d6658e
sohail-op <67971304+sohail-op@users.noreply.github.com>
2025-02-15 03:36:38 +0530

    misc: change orgin from "https://tshare-frontend.vercel.app/" to "*"

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://tshare-frontend.vercel.app/",
+    origin: "*",
     methods: ["GET", "POST", "DELETE"],
   },
 });
