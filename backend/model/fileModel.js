dc58a804f892dd97bf3871c1bf7f340a4d8f1a9d
sohail-op <67971304+sohail-op@users.noreply.github.com>
2025-02-21 04:58:54 +0530

    Add: auto delete after 30 min in MongoDB

--- backend/model/fileModel.js
+++ backend/model/fileModel.js
@@ -5,9 +5,15 @@ const fileSchema = new mongoose.Schema(
     text: { type: String, required: [true, "Please  provide text content"] },
 
     code: { type: String, required: [true, "Code is missing"], unique: true },
+
+    expiresAt: {
+      type: Date,
+      default: () => new Date(Date.now() + 30 * 60 * 1000), // Set expiry time (15 mins)
+      index: { expires: "30m" }, // TTL index for auto-deletion
+    },
   },
   {
-    timestamps: true, // Saves createdAt and updatedAt as dates. Creates them in ISO 8601 format yyyy-mm-ddTHH:MM:SS.ssstimestamps: true});
+    timestamps: true,
   }
 );
 
