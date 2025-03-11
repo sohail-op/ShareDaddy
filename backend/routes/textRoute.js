d9e9ccf1bcf84b4df0df0049d1ae12ed3fbd081f
sohail-op <67971304+sohail-op@users.noreply.github.com>
2025-03-11 23:53:16 +0530

    minor change

--- backend/routes/textRoute.js
+++ backend/routes/textRoute.js
@@ -1,19 +1,11 @@
 import express from "express";
-import multer from "multer";
 
+import {upload} from "../middleware/multerStorageHandler.js";
 import {
   uploadText, uploadFile, getTextOrFile
 } from "../controllers/textController.js";
 
 const router = express.Router();
-
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-const upload = multer({ storage });
 
 router.route("/uploadText").post(uploadText);
 router.route("/uploadFile").post( upload.single("file"), uploadFile);
